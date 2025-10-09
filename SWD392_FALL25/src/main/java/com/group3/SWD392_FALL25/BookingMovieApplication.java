45d4b9040cb10ec537a3b232d494c610772073ef
LDT <thangle84264@gmail.com>
2025-10-09 14:49:25 +0700

    Start project

---
+++ SWD392_FALL25/src/main/java/com/group3/SWD392_FALL25/BookingMovieApplication.java
@@ -0,0 +1,13 @@
+package com.group3.SWD392_FALL25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingMovieApplication.class, args);
+	}
+
+}
---
+++ SWD392_FALL25/src/test/java/com/group3/SWD392_FALL25/BookingMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group3.SWD392_FALL25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
